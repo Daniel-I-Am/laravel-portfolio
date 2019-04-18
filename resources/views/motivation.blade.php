29d5d7ce3f2b2a6c064afbb8364391769c930158
Daniel de Cloet <djdecloet@gmail.com>
2019-04-18 09:02:37 +0200

    Add other static pages

--- resources/views/motivation.blade.php
+++ resources/views/motivation.blade.php
@@ -1,5 +1,98 @@
 @extends('layout.base')
 
 @section('body')
-    <p>asdasd</p>
+    <div class="container">
+        <div class="row">
+            <div class="card-item-outer col-6">
+                <div class="card-item">
+                    <h2 class=title>Theorie</h2>
+                    <p class=content>
+                        Motivatie, in kort, is de wil om iets te doen, of het gevoel nodig te zijn voor iets.
+                        Wanneer het is afgemaakt, zou een gevoel van tevredenheid gevoeld moeten worden.
+                        Motivatie zorgt ervoor dat dingen afgemaakt worden.
+                        <br />
+                        <br />
+                        <strong>Ted Talk</strong>
+                        <br />
+                        <i>Autonomy, mastery and purpose, the building blocks of a new way of doing things.</i>
+                        Het belangrijkste punt, volgens Dan Pink, is om zelf te kunnen beslissen, om je werk zeer goed te beheersen en om een doel in je werk te kunnen vinden.
+                        Zeker het eerste punt, <i>autonomy</i>, is zeer belangrijk; hier gaat Dan Pink dan ook veel op in. Je moet het werk je eigen maken, zodat je er
+                        zelf graag tijd aan gaat besteden.
+                        <br />
+                        <br />
+                        <strong>Argumenten</strong>
+                        <br />
+                        <i>testnet.org</i>
+                        <br />
+                        <i>Testers willen meer autonomie en zijn op zoek naar hogere zingeving.</i> Dit komt precies overeen met wat Dan Pink zei in zijn talk.
+                        Namelijk <i>autonomy</i>, zorgen dat je werk je eigen is.
+                        <br />
+                        Dit artikel gaat ook verder in, met voorbeelden om deze autonomie te bevorderen, verder ingaand op de punten die Dan Pink ook probeerde te maken.
+                        <br />
+                        <br />
+                        <i>carrieretijger.nl</i>
+                        <br />
+                        <i>Zorg voor uitdagende doelen</i>. Zeer van zichzelf sprekend, uitdagende doelen laten je vastbijten in een onderwerp.
+                        <br />
+                        <i>Geef positieve feedback en heb aandacht voor prestaties.</i> Positieve feedback zorgt ervoor dat risico's genomen kunnen worden. Zelfs als iets faalt moet daar toch het goede van worden ingezien.
+                        <br />
+                        Dit artikel geeft een wat meer <i>step-by-step guide</i> om een betere motivatie te krijgen.
+                    </p>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="card-item-outer">
+                    <div class="card-item">
+                        <h2 class=title>Prestatie Motivatie</h2>
+                        <p class=content>
+                            Je bent in <strong>redelijke mate intrinsiek gemotiveerd</strong> en maar <strong>weinig extrinsiek gemotiveerd</strong>.
+                            Dat je redelijk intrinsiek gemotiveerd bent betekent dat je het best leuk vindt om te studeren.
+                            Maar héél leuk vind je het nou ook weer niet. Dat je weinig extrinsiek gemotiveerd bent wil zeggen dat het je weinig kan
+                            schelen wat je met je studie kunt bereiken: een diploma, geld, status of de erkenning van de mensen waar je van houdt.
+                            <strong>Je interesses zijn voor jou belangrijker.</strong>
+                        </p>
+                    </div>
+                </div>
+                <div class="card-item-outer">
+                    <div class="card-item">
+                        <h2 class=title>Carrièreankers</h2>
+                        <p class=content>
+                            Eerste Plek: Onafhankelijkheid
+                            <br />
+                            Een van de meest belangrijkste zaken voor mij, Onafhankelijkheid is eigenlijk deels een consequentie van autonomie.
+                            Ik vind het heel fijn om te kunnen doen wat ik wil. Hier betekent <i>autonomie</i> de mogelijkheid om mijn eigen keuzes te maken.
+                            <br />
+                            <br />
+                            Tweede Plek: Zekerheid
+                            <br />
+                            Dit is een verassend iets voor mij, ik hou wel van zekerheid, maar dit realiseer ik niet bewust.
+                            Het is moeilijk hier een voorbeeld bij te geven sinds het een nieuw punt voor mij is.
+                            Ik vind het altijd wel fijn om te weten waar ik in stap. Maar ik zou het zelf niet zo hoog op de lijst zetten.
+                            <br />
+                            <br />
+                            Minst: Sociale contacten
+                            <br />
+                            Voor de meesten zal dit heel ergens anders staan, maar voor mij niet. Al is natuurlijk het fijn om mensen rond me te hebben, maar echt belangrijk vind ik het niet.
+                            Zonder anderen, voornamelijk hun afleiding, kan ik me concentreren op dat wat ik interessant vind.
+                            Ik heb het zogenoemde <i>INTP</i>-personaliteits type, met dit gegeven is het duidelijk waarom dit anker zo laag staat.
+                            Zoals in het beneden gelinkte artikel over dit personaliteits type staat: "<i>Further, with Thinking (T) as one of their governing traits, INTPs are unlikely to understand emotional complaints at all, and their friends won’t find a bedrock of emotional support in them.</i>"
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="card-item-outer col-12">
+                    <div class="card-item">
+                        <h2 class=title>Conclusie</h2>
+                        <p class=content>
+                            Ik ben een zeer gedreven student, zeer gemotiveerd. Dit is simpelweg omdat ik het ICT-veld, grote delen er van in ieder geval, zeer interessant vind.
+                            Mijn motivatie is niet zo zeer gedreven door <i>purpose</i>, zoals Dan Pink vertelde, maar wel heel erg door de andere twee aspecten die hij noemde, namelijk <i>autonomy</i> en <i>mastery</i>.
+                            <br />
+                            <i>Feedback ouders</i>
+                            Zij zijn het zeker eens met het feit dat ik behoorlijk gemotiveerd kan zijn. Zij geven zelfs aan dat ik me meestal heel erg vast bijt, als ik ergens aan begin. Soms zo erg dat ik me niet eens realiseer hoe lang ik ergens aan bezig ben, omdat ik alle andere dingen gewoon laat vallen.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
 @endsection
--- resources/views/vision.blade.php
+++ resources/views/vision.blade.php
@@ -1,5 +1,44 @@
 @extends('layout.base')
 
 @section('body')
-    <p>asdasd</p>
+    <div class="container">
+        <div class="row">
+            <div class="card-item-outer col-6">
+                <div class="card-item">
+                    <h2 class=title>HBO-I Job Event</h2>
+                    <p class=content>
+                        Bij de bedrijven met welke ik gesproken heb, worden meerdere culturen gebruikt. Zo wordt bij Accenture gewerkt in projectgroepen,
+                        deze project groepen zijn iedere keer anders. Bij dongIT en Secura BV, welke pentesting bedrijven zijn, wordt natuurlijk op een heel andere manier
+                        gedacht. Hierbij wordt voornamelijk gekeken naar <i>hoe</i> een werkend programma/applicatie gebroken kan worden door een mogelijke hacker.
+                        Bij de Sligro wordt veel geoutsourced op dit moment, dat betekent dat er toch meer een management cultuur hangt.
+                        <br><br>
+                        Er zijn vele functies binnen de bedrijven. Bij Quintor zijn de meeste posities full-stack developers, terwijl bij Accenture de posities opgesplitst zijn
+                        in front-end, back-end, management, consulting, etc. Bij de pentesting bedrijven, dongIT en Secura BV, is de belangrijkste positie een pentester, maar ook zijn er een aantal posities die meer
+                        met de mensen omgaan, social engineering posities.
+                        <br>
+                        Ook zijn er vele bedrijven die server beheer posities hebben; de lieden die het bedrijf werkelijk in werking houden.
+                        <br><br>
+                        Ten slotte zijn er ook vele manier van werken in de verschillende bedrijven. Zoals eerder genoemd, werkt de Sligro met out-sourcing, zelf doen ze dus niet zo heel veel, maar ze laten andere
+                        bedrijven het werk doen. Een aantal bedrijven werken met de Agile methode, hierbij dan ook Scrum; deze bedrijven zijn bijvoorbeel Quintor of Accenture.
+                    </p>
+                </div>
+            </div>
+            <div class="card-item-outer col-6">
+                <div class="card-item">
+                    <h2 class=title>Bedrijvensafari</h2>
+                    <p class=content>
+                        Bij de bedrijvensafari op 11 oktober ben ik langsgeweest bij Syntess, Omoda, Your Surprise, ZLM en Soltegro.
+                        <br>
+                        Veel dingen van het Job Event kwamen hier ook weer terug. Zo werkt Syntess met Agile/Scrum. De ZLM werkte met de waterval methode, maar wisselt ook over naar Agile.
+                        <br>
+                        De culturen binnen de bedrijven variëren wel behoorlijk. Zo is Your Surprise een bedrijf dat enorm aan het uitbreiden is; wat over aan het gaan is van klein naar groot bedrijf.
+                        <img src="./assets/omoda.jpg">
+                        Op 19 december bedrijfsbezoek aan Elloro/Recranet, Damen en Conneqtech te Vlissingen.
+                        <br>
+                        Deze bedrijven hebben we bezocht om een <i>real life</i> implementatie te kunnen zien van een aantal van de technieken die we nu in de lessen leren.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
