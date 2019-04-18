b0f1f541674e28b0ac805ee173f8d7108175c2e8
Daniel de Cloet <djdecloet@gmail.com>
2019-04-19 00:52:23 +0200

    Add credit_obtained_count attribute

--- app/Course.php
+++ app/Course.php
@@ -10,7 +10,7 @@ class Course extends Model
         'name', 'term'
     ];
 
-    protected $appends = ['credit_count'];
+    protected $appends = ['credit_count', 'credit_obtained_count'];
 
     public function subjects() {
         return $this->hasMany(Subject::class);
@@ -24,6 +24,21 @@ class Course extends Model
         $total = 0;
         foreach ($this->subjects as $subject)
             $total += $subject->ec_value;
+        return $total;
+    }
+
+    public function getCreditObtainedCountAttribute() {
+        $total = 0;
+        foreach ($this->subjects as $subject) {
+            $ecs = $subject->ec_value;
+            foreach ($subject->grades as $grade) {
+                if ($grade->grade == null || ($grade->lowest_passing != null && $grade->grade < $grade->lowest_passing))
+                    $ecs = 0;
+            }
+            if ($subject->grades->count() == 0 || array_reduce(array_map(function ($value) { return $value['grade']; }, $subject->grades->toArray()), function ($sum, $elem) { return $sum + $elem; }) / $subject->grades->count() < 5.5)
+                $ecs = 0;
+            $total += $ecs;
+        }
         return $total;
     }
 }
