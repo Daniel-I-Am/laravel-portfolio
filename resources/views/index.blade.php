96d7c5b6cfb87d0fdaac1586ae8ae89e38871d7d
Daniel de Cloet <djdecloet@gmail.com>
2019-04-18 08:51:53 +0200

    Add index page with new cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,46 +3,52 @@
 @section('body')
     <div class="container">
         <div class="row">
-            <div class="card-item col-6">
-                <h2 class="title">Achtergrond</h2>
-                <div class="content">
-                    <p>
-                        Mijn naam is Daniel de Cloet. Ik ben, op het moment van schrijven, 17 jaar oud. Ik heb hiervoor een
-                        VWO/Gymnasium diploma behaald. Deze heb ik behaald in een zeer technisch vakkenpakket, of zoals ik het noem,
-                        mijn versie van een pret-pakket.
-                    </p>
+            <div class="card-item-outer col-6">
+                <div class="card-item">
+                    <h2 class="title">Achtergrond</h2>
+                    <div class="content">
+                        <p>
+                            Mijn naam is Daniel de Cloet. Ik ben, op het moment van schrijven, 17 jaar oud. Ik heb hiervoor een
+                            VWO/Gymnasium diploma behaald. Deze heb ik behaald in een zeer technisch vakkenpakket, of zoals ik het noem,
+                            mijn versie van een pret-pakket.
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div class="card-item col-6">
-                <h2 class="title">Waarom HBO</h2>
-                <div class="content">
-                    <p>
-                        Vaak krijg ik toch de vraag "Waarom heb je dan geen WO studie gedaan". Het antwoord daarop is
-                        misschien simpeler dan gedacht. Er zit nog al een verschil tussen een WO en HBO studie.
-                        Voor een opleiding in de richting ICT is een HBO opleiding beter dan een WO opleiding,
-                        in mijn opzicht. Een iets minder theoretische opleiding helpt om meer hands-on ervaring op
-                        te bouwen. Al heb ik al best wat ervaring, meer kan nooit kwaad.
-                    </p>
+            <div class="card-item-outer col-6">
+            <div class="card-item">
+                    <h2 class="title">Waarom HBO</h2>
+                    <div class="content">
+                        <p>
+                            Vaak krijg ik toch de vraag "Waarom heb je dan geen WO studie gedaan". Het antwoord daarop is
+                            misschien simpeler dan gedacht. Er zit nog al een verschil tussen een WO en HBO studie.
+                            Voor een opleiding in de richting ICT is een HBO opleiding beter dan een WO opleiding,
+                            in mijn opzicht. Een iets minder theoretische opleiding helpt om meer hands-on ervaring op
+                            te bouwen. Al heb ik al best wat ervaring, meer kan nooit kwaad.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="card-item col-12">
-                <h2 class="title">Waarom de opleiding ICT</h2>
-                <p class="content">Ik ben al zeer lange tijd bezig geweest met allerlei onderdelen van de ICT wereld.<br>Hetgeen het
-                    meeste tijd opneemt is het zijn van server admin van game servers. Ik speel MineCraft op servers, natuurlijk wel
-                    met heel veel <span class="tooltip">mods<span class="tooltiptext"><strong>Mods</strong> (short for
-                        <strong>modifications</strong>) are anything that changes <i>MineCraft's</i> game content from what it
-                        originally was.</span></span>. Natuurlijk komt er best was bij kijken, naast alles wat in de game zelf
-                    moet gebeuren, moet ook de server onderhouden worden, moeten discord bots het doen, moet een admin-only panel
-                    toegankelijk zijn voor de wat minder technisch aangelegde server owner, en zo voorts. Dit doe ik met heel erg veel
-                    plezier.<br>Daarnaast heb ik ook zeer veel geprogrammeers in allerlei talen. De taal die ik het allerbeste ken
-                    moet <a href="https://www.lua.org/">LUA</a> zijn. Daarna komen, niet noodzakelijk in volgorde van bekendheid en
-                    vertrouwdheid, C#, welke ik heb gedaan in een game genaamd <a href="https://www.spaceengineersgame.com/"><i>Space
-                            Engineers</i></a>, BASH and PERL, allebei een consequentie van het draaien van Linux op mijn devices,
-                    <a href="https://getfedora.org/">Fedora</a> op mijn laptop en <a href="https://www.archlinux.org/">Arch</a> op
-                    mijn desktop computer. Tenslotte de web-development talen, JavaScript en PHP, maar ook python, allemaal door
-                    projecten voor en met een internationale groep vrienden.</p>
+            <div class="card-item-outer col-12">
+                <div class="card-item">
+                    <h2 class="title">Waarom de opleiding ICT</h2>
+                    <p class="content">Ik ben al zeer lange tijd bezig geweest met allerlei onderdelen van de ICT wereld.<br>Hetgeen het
+                        meeste tijd opneemt is het zijn van server admin van game servers. Ik speel MineCraft op servers, natuurlijk wel
+                        met heel veel <span class="tooltip">mods<span class="tooltiptext"><strong>Mods</strong> (short for
+                            <strong>modifications</strong>) are anything that changes <i>MineCraft's</i> game content from what it
+                            originally was.</span></span>. Natuurlijk komt er best was bij kijken, naast alles wat in de game zelf
+                        moet gebeuren, moet ook de server onderhouden worden, moeten discord bots het doen, moet een admin-only panel
+                        toegankelijk zijn voor de wat minder technisch aangelegde server owner, en zo voorts. Dit doe ik met heel erg veel
+                        plezier.<br>Daarnaast heb ik ook zeer veel geprogrammeers in allerlei talen. De taal die ik het allerbeste ken
+                        moet <a href="https://www.lua.org/">LUA</a> zijn. Daarna komen, niet noodzakelijk in volgorde van bekendheid en
+                        vertrouwdheid, C#, welke ik heb gedaan in een game genaamd <a href="https://www.spaceengineersgame.com/"><i>Space
+                                Engineers</i></a>, BASH and PERL, allebei een consequentie van het draaien van Linux op mijn devices,
+                        <a href="https://getfedora.org/">Fedora</a> op mijn laptop en <a href="https://www.archlinux.org/">Arch</a> op
+                        mijn desktop computer. Tenslotte de web-development talen, JavaScript en PHP, maar ook python, allemaal door
+                        projecten voor en met een internationale groep vrienden.</p>
+                </div>
             </div>
         </div>
     </div>
