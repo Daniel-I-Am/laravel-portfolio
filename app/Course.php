91b62dcf04ca88bdee2d7c5d45e4a555f2d51592
Daniel de Cloet <djdecloet@gmail.com>
2019-04-19 00:38:07 +0200

    Add credit count to append array

--- app/Course.php
+++ app/Course.php
@@ -10,11 +10,20 @@ class Course extends Model
         'name', 'term'
     ];
 
+    protected $appends = ['credit_count'];
+
     public function subjects() {
         return $this->hasMany(Subject::class);
     }
 
     public function grades() {
         return $this->with(['subjects', 'subjects.grades']);
+    }
+
+    public function getCreditCountAttribute() {
+        $total = 0;
+        foreach ($this->subjects as $subject)
+            $total += $subject->ec_value;
+        return $total;
     }
 }
